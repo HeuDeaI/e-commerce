63e1a6c6c6d8b002bc5506875074c4302926e382
Tsimka <p.profitpt@gmail.com>
2025-04-30 19:52:21 +0300

    add frontend supporting

--- cmd/e-commerce/main.go
+++ cmd/e-commerce/main.go
@@ -12,6 +12,7 @@ import (
 	"e-commerce/internal/database"
 	"e-commerce/internal/product"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -48,6 +49,15 @@ func main() {
 	categoryHandler := category.NewCategoryHandler(categoryService)
 
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	productHandler.RegisterRoutes(router)
 	brandHandler.RegisterRoutes(router)
